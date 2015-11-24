46e6e24c25c3e42218473ac5624d8a23b2835704
Alberto Penas Amor <alberto__pa@hotmail.com>
2015-11-24 23:55:57 +0100

    added usecase cancelables

--- app/src/main/java/sw/es/domain/repository/repo/usecase/FetchUseCase.java
+++ app/src/main/java/sw/es/domain/repository/repo/usecase/FetchUseCase.java
@@ -2,6 +2,10 @@ package sw.es.domain.repository.repo.usecase;
 
 import javax.inject.Inject;
 
+import rx.Subscription;
+import sw.es.dagger2.BuildConfig;
+import sw.es.domain.repository.repo.exception.NoMoreCriteriaException;
+import sw.es.domain.repository.repo.exception.NotFoundInDataStoreException;
 import sw.es.domain.repository.repo.repository.LoadCallback;
 import sw.es.domain.repository.repo.criteria.LoadCriteria;
 import sw.es.domain.repository.repo.repository.Repository;
@@ -9,10 +13,12 @@ import sw.es.domain.repository.repo.repository.Repository;
 /**
  * Created by albertopenasamor on 21/10/15.
  */
-public class FetchUseCase<Model, Params> implements UseCase<Params>{
+public class FetchUseCase<Model, Params> implements UseCase<Params>, LoadCallback<Model, Params>{
 
 
     private Repository<Model, Params> repository;
+    private UseCaseCallback callback;
+    private Subscription subscription;
 
 
     @Inject
@@ -23,16 +29,49 @@ public class FetchUseCase<Model, Params> implements UseCase<Params>{
 
     @Override
     public void run(Params params, final UseCaseCallback callback) {
-        repository.fetch(params, LoadCriteria.newGet(), new LoadCallback<Model, Params>() {
-            @Override
-            public void onFetch(Params params, LoadCriteria loadCriteria, Model model) {
-                callback.onResult(params, model);
-            }
+        this.callback = callback;
+        subscription = repository.fetch(params, LoadCriteria.newGet(), this);
+    }
 
-            @Override
-            public void onFetchError(Params params, LoadCriteria loadCriteria, Throwable throwable) {
+
+    @Override
+    public void cancel() {
+        if (hasSubscription() && isSubscribed()){
+            subscription.unsubscribe();
+        }
+    }
+
+
+    @Override
+    public void onFetch(Params params, LoadCriteria loadCriteria, Model model) {
+        callback.onResult(params, model);
+    }
+
+
+    @Override
+    public void onFetchError(Params params, LoadCriteria loadCriteria, Throwable throwable) {
+        if (BuildConfig.DEBUG){
+            throwable.printStackTrace();
+        }
+        if (throwable instanceof NotFoundInDataStoreException) {
+            try {
+                LoadCriteria newLoadCriteria = loadCriteria.next();
+                subscription = repository.fetch(params, newLoadCriteria, this);
+            } catch (NoMoreCriteriaException e) {
                 callback.onResultError(params, throwable);
             }
-        });
+        }else{
+            callback.onResultError(params, throwable);
+        }
+    }
+
+
+    private boolean hasSubscription(){
+        return subscription != null;
+    }
+
+
+    private boolean isSubscribed(){
+        return !subscription.isUnsubscribed();
     }
 }
